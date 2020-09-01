6c0531d482633a5991cde12a630615ee6995837b
pratikpc <pratikpc@users.noreply.github.com>
2020-09-01 20:26:22 +0000

    deploy: 378471aeaede63a125e73bf5c10b4e382d8ac206

---
+++ precache-manifest.60778e0ad60b4ee637c39a204bce07fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d976e7d985a61f910106ff355de5842b",
+    "url": "/Battery-Low-Notifier/index.html"
+  },
+  {
+    "revision": "f7c7f0d448c3607792f8",
+    "url": "/Battery-Low-Notifier/static/css/main.fb8e7c72.chunk.css"
+  },
+  {
+    "revision": "e5ad4cd1cf794d4a4391",
+    "url": "/Battery-Low-Notifier/static/js/2.570e0d15.chunk.js"
+  },
+  {
+    "revision": "6c661bfd12b151c09a654fe75d1650f7",
+    "url": "/Battery-Low-Notifier/static/js/2.570e0d15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7c7f0d448c3607792f8",
+    "url": "/Battery-Low-Notifier/static/js/main.344e5894.chunk.js"
+  },
+  {
+    "revision": "cead30af5ef70e3f2216be33287084ff",
+    "url": "/Battery-Low-Notifier/static/js/notify.worker.0637b768.worker.js"
+  },
+  {
+    "revision": "db6ddcc39171d302ad4d0a977b136dc0",
+    "url": "/Battery-Low-Notifier/static/js/notify.worker.0637b768.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bba3e2dd7104c7e801e",
+    "url": "/Battery-Low-Notifier/static/js/runtime-main.de316204.js"
+  }
+]);
